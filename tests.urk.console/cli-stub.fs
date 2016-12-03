1e4e622d8daa3169dfb074c5d45e4ff319190ab7
ned rihine <ned.rihine@gmail.com>
2016-12-04 00:42:00 +0900

    add project tests.urk.console

---
+++ tests.urk.console/cli-stub.fs
@@ -0,0 +1,16 @@
+﻿module Urk.CliStub
+
+open Argu
+
+type UrkStubArg =
+  | [<AltCommandLine("-f")>] File                of string
+  | [<AltCommandLine("-V")>] Version
+  with
+    interface IArgParserTemplate with
+      member this.Usage =
+        match this with
+         | File _                 -> ""
+         | Version                -> ""
+
+let create_parser () =
+  ArgumentParser.Create<UrkStubArg>()
---
+++ tests.urk.console/cli-test.fs
@@ -0,0 +1,20 @@
+﻿module Urk.Console.Tests
+
+open Argu
+open NUnit.Framework
+
+open Urk.CliStub
+
+
+[<TestFixture>]
+type CliTest() = class
+
+  [<Test>]
+  member self.Test01() =
+    let parser = create_parser()
+
+    //Assert.Throws<ArguParseException>( fun _ -> ignore ( parser.Parse( [| "--help" |] ) ) )
+    let results = parser.Parse( [| "--help" |] )
+
+    Assert.IsTrue( results.IsUsageRequested )
+end
